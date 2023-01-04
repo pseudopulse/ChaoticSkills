a069a1c41dfdc3fe25f920ade489a04d7a79027a
pseudopulse <psuedopulse@protonmail.com>
2023-01-03 20:47:04 -0600

    thermal thruster but engineer

---
+++ ChaoticSkills/Content/Engineer/Thruster.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ChaoticSkills.Content.Engineer {
+    public class Thruster : SkillBase<Thruster> {
+        public override SerializableEntityStateType ActivationState => ContentAddition.AddEntityState<EntityStates.Engineer.Thruster>(out bool _);
+        public override float Cooldown => 7f;
+        public override bool DelayCooldown => false;
+        public override string Description => "<style=cIsDamage>Stunning</style>. <style=cIsDamage>Resilient</style>. Launch into the air and burst forward after a delay, dealing <style=cIsDamage>100%-2500%</style> damage upon impact scaling with <style=cIsUtility>fall speed</style>.";
+        public override bool Agile => true;
+        public override bool IsCombat => true;
+        public override string LangToken => "Thruster";
+        public override int MaxStock => 2;
+        public override UnlockableDef Unlock => null;
+        public override string Machine => "Weapon";
+        public override Sprite SkillIcon => Main.Assets.LoadAsset<Sprite>("Assets/Icons/Engineer/Thruster.png");
+        public override SkillSlot Slot => SkillSlot.Utility;
+        public override string Survivor => Utils.Paths.GameObject.EngiBody;
+        public override string Name => "Overdrive Boost";
+        public override List<string> Keywords => new() { Utils.Keywords.Stun, "KEYWORD_RESILIENT" };
+
+        public override void PostCreation()
+        {
+            base.PostCreation();
+            "KEYWORD_RESILIENT".Add("<style=cKeywordName>Resilient</style>You are <style=cIsUtility>immune to fall damage</style> during a <style=cIsDamage>Resilient</style> skill.");
+        }
+
+    }
+}
---
+++ ChaoticSkills/EntityStates/Engineer/Thruster.cs
@@ -0,0 +1,75 @@
+using System;
+using EntityStates.Merc.Weapon;
+
+namespace ChaoticSkills.EntityStates.Engineer {
+    public class Thruster : BaseState {
+        private float forwardForce = 2900f;
+        private float upwardForce = 1700f;
+        private float upwardDelay = 0.7f;
+        private bool hasThrusted = false;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            base.characterMotor.ApplyForce(Vector3.up * upwardForce, true);
+            AkSoundEngine.PostEvent(Events.Play_item_use_BFG_explode, base.gameObject);
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            On.RoR2.GlobalEventManager.OnCharacterHitGroundServer -= Impact;
+        }
+
+        private void Impact(On.RoR2.GlobalEventManager.orig_OnCharacterHitGroundServer orig, GlobalEventManager self, CharacterBody body, Vector3 impactVelocity) {
+            if (body == base.characterBody) {
+                orig(self, body, Vector3.zero);
+                float damageCoeff = Mathf.Clamp(1f + (Mathf.Abs(impactVelocity.y * 5) * 0.3f), 1f, 25f);
+                float radiusCoeff = Mathf.Clamp(1f + (Mathf.Abs(impactVelocity.y * 5) * 0.3f), 1f, 3f);
+                Debug.Log(base.damageStat * damageCoeff);
+                BlastAttack attack = new();
+                attack.damageType = DamageType.Stun1s;
+                attack.baseDamage = base.damageStat * damageCoeff;
+                attack.radius = 3.5f * radiusCoeff;
+                attack.position = body.corePosition;
+                attack.crit = base.RollCrit();
+                attack.damageType = DamageType.Stun1s;
+                attack.attackerFiltering = AttackerFiltering.NeverHitSelf;
+                attack.losType = BlastAttack.LoSType.None;
+                attack.falloffModel = BlastAttack.FalloffModel.Linear;
+                attack.baseForce = 500f;
+                attack.procCoefficient = 1f;
+                attack.teamIndex = base.GetTeam();
+                attack.inflictor = base.gameObject;
+                attack.attacker = base.gameObject;
+
+                attack.Fire();
+
+                EffectManager.SpawnEffect(Utils.Paths.GameObject.OmniImpactVFX.Load<GameObject>(), new EffectData {
+                    scale = radiusCoeff,
+                    origin = body.corePosition,
+                    rotation = Quaternion.identity,
+                }, true);
+
+                AkSoundEngine.PostEvent(Events.Play_item_proc_fallboots_impact, base.gameObject);
+
+                outer.SetNextStateToMain();
+            }
+            else {
+                orig(self, body, impactVelocity);
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (base.fixedAge >= upwardDelay && !hasThrusted) {
+                On.RoR2.GlobalEventManager.OnCharacterHitGroundServer += Impact;
+                hasThrusted = true;
+                base.characterMotor.velocity = Vector3.zero;
+                base.characterMotor.ApplyForce((base.GetAimRay().direction) * forwardForce, true);
+                AkSoundEngine.PostEvent(Events.Play_item_use_BFG_explode, base.gameObject);
+            }
+        }
+    }
+}
