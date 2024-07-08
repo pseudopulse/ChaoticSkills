8e777adbf2545dd9ec95c2a2f924c29da69150da
Bluagchmied <102863527+Bludstonian@users.noreply.github.com>
2024-07-07 22:42:40 -0400

    Update CallBackup.cs
    More of the survivors added.

--- ChaoticSkills/EntityStates/Captain/CallBackup.cs
+++ ChaoticSkills/EntityStates/Captain/CallBackup.cs
@@ -15,7 +15,14 @@ namespace ChaoticSkills.EntityStates.Captain {
                     Utils.Paths.GameObject.TreebotMonsterMaster.Load<GameObject>(),
                     Utils.Paths.GameObject.LoaderMonsterMaster.Load<GameObject>(),
                     Utils.Paths.GameObject.CommandoMonsterMaster.Load<GameObject>(),
-                    Utils.Paths.GameObject.Bandit2MonsterMaster.Load<GameObject>()
+                    Utils.Paths.GameObject.Bandit2MonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.CaptainMonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.RailgunnerMonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.VoidSurvivorMonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.MageMonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.HuntressMonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.EngineerMonsterMaster.Load<GameObject>(),
+                    Utils.Paths.GameObject.CrocoMonsterMaster.Load<GameObject>()
                 };
 
                 MasterSummon summon = new();
